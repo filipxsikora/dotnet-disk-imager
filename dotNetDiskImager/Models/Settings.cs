cc85710806603725f23e1ed0b1bb16dae9049c8c
Filip Sikora <filipxsikora@gmail.com>
2016-04-21 18:01:56 +0200

    Implemented Settings and SettingsWindow logic

---
+++ dotNetDiskImager/Models/Settings.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace dotNetDiskImager.Models
+{
+    public enum DefaultFolder { LastUsed, UserDefined }
+    public enum Animations { AlwaysOn, OnlyOnAcPower, Never }
+    public enum TaskbarExtraInfo { Nothing, Percent, CurrentSpeed, RemainingTime, ActiveDevice, ImageFileName }
+
+    public static class AppSettings
+    {
+        public static SettingsInternal Settings { get; set; } = SettingsInternal.Default;
+
+        public static void SaveSettings()
+        {
+            string directory = string.Format(@"{0}\dotNetDiskImager", Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData));
+            if (!Directory.Exists(directory))
+                Directory.CreateDirectory(directory);
+            using (var stream = File.Create(string.Format(@"{0}\settings.xml", directory )))
+            {
+                var serializer = new XmlSerializer(typeof(SettingsInternal));
+                serializer.Serialize(stream, Settings);
+            }
+        }
+
+        public static void LoadSettings()
+        {
+            string file = string.Format(@"{0}\dotNetDiskImager\settings.xml", Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData));
+            if (!File.Exists(file))
+                return;
+            using (var stream = File.Open(file, FileMode.Open))
+            {
+                var serializer = new XmlSerializer(typeof(SettingsInternal));
+                Settings = serializer.Deserialize(stream) as SettingsInternal;
+            }
+        }
+    }
+
+    public class SettingsInternal
+    {
+        public bool DisplayWriteWarnings { get; set; }
+        public DefaultFolder DefaultFolder { get; set; }
+        public string DefaultFolderPath { get; set; }
+        public Animations AnimationsSettings { get; set; }
+        public TaskbarExtraInfo TaskbarExtraInfo { get; set; }
+
+        public static SettingsInternal Default
+        {   get
+            {
+                return new SettingsInternal()
+                {
+                    AnimationsSettings = Animations.AlwaysOn,
+                    DefaultFolder = DefaultFolder.LastUsed,
+                    DefaultFolderPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments),
+                    DisplayWriteWarnings = true,
+                    TaskbarExtraInfo = TaskbarExtraInfo.Nothing
+                };
+            }
+        } 
+    }
+}
--- dotNetDiskImager/SettingsWindow.xaml.cs
+++ dotNetDiskImager/SettingsWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using dotNetDiskImager.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,6 +23,158 @@ namespace dotNetDiskImager
         public SettingsWindow()
         {
             InitializeComponent();
+
+            LoadAppSettingsToGUI();
+        }
+
+        void LoadAppSettingsToGUI()
+        {
+            displayPreWriteWarnings.IsChecked = AppSettings.Settings.DisplayWriteWarnings;
+
+            if (AppSettings.Settings.DefaultFolder == DefaultFolder.LastUsed)
+            {
+                defaultFolder_lastUsed.IsChecked = true;
+                defaultFolder_userSpecify.IsChecked = false;
+                defaultFolderDialogButton.IsEnabled = false;
+                defaultFolder_userSpecifyValue.IsEnabled = false;
+            }
+            else
+            {
+                defaultFolder_lastUsed.IsChecked = false;
+                defaultFolder_userSpecify.IsChecked = true;
+                defaultFolderDialogButton.IsEnabled = true;
+                defaultFolder_userSpecifyValue.IsEnabled = true;
+                defaultFolder_userSpecifyValue.Text = AppSettings.Settings.DefaultFolderPath;
+            }
+
+            switch (AppSettings.Settings.AnimationsSettings)
+            {
+                case Animations.AlwaysOn:
+                    animationsOptions.SelectedIndex = 0;
+                    break;
+                case Animations.OnlyOnAcPower:
+                    animationsOptions.SelectedIndex = 1;
+                    break;
+                case Animations.Never:
+                    animationsOptions.SelectedIndex = 2;
+                    break;
+            }
+
+            switch (AppSettings.Settings.TaskbarExtraInfo)
+            {
+                case TaskbarExtraInfo.Nothing:
+                    showMoreOptions.SelectedIndex = 0;
+                    break;
+                case TaskbarExtraInfo.Percent:
+                    showMoreOptions.SelectedIndex = 1;
+                    break;
+                case TaskbarExtraInfo.CurrentSpeed:
+                    showMoreOptions.SelectedIndex = 2;
+                    break;
+                case TaskbarExtraInfo.RemainingTime:
+                    showMoreOptions.SelectedIndex = 3;
+                    break;
+                case TaskbarExtraInfo.ActiveDevice:
+                    showMoreOptions.SelectedIndex = 4;
+                    break;
+                case TaskbarExtraInfo.ImageFileName:
+                    showMoreOptions.SelectedIndex = 5;
+                    break;
+            }
+        }
+
+        private void defaultFolder_lastUsed_Click(object sender, RoutedEventArgs e)
+        {
+            defaultFolderDialogButton.IsEnabled = false;
+            defaultFolder_userSpecifyValue.IsEnabled = false;
+        }
+
+        private void defaultFolder_userSpecify_Click(object sender, RoutedEventArgs e)
+        {
+            defaultFolderDialogButton.IsEnabled = true;
+            defaultFolder_userSpecifyValue.IsEnabled = true;
+        }
+
+        private void saveButton_Click(object sender, RoutedEventArgs e)
+        {
+            AppSettings.Settings.DisplayWriteWarnings = displayPreWriteWarnings.IsChecked.Value;
+
+            if (defaultFolder_lastUsed.IsChecked.Value)
+            {
+                AppSettings.Settings.DefaultFolder = DefaultFolder.LastUsed;
+            }
+            else
+            {
+                AppSettings.Settings.DefaultFolder = DefaultFolder.UserDefined;
+                AppSettings.Settings.DefaultFolderPath = defaultFolder_userSpecifyValue.Text;
+            }
+
+            switch (animationsOptions.SelectedIndex)
+            {
+                case 0:
+                    AppSettings.Settings.AnimationsSettings = Animations.AlwaysOn;
+                    break;
+                case 1:
+                    AppSettings.Settings.AnimationsSettings = Animations.OnlyOnAcPower;
+                    break;
+                case 2:
+                    AppSettings.Settings.AnimationsSettings = Animations.Never;
+                    break;
+            }
+
+            switch (showMoreOptions.SelectedIndex)
+            {
+                case 0:
+                    AppSettings.Settings.TaskbarExtraInfo = TaskbarExtraInfo.Nothing;
+                    break;
+                case 1:
+                    AppSettings.Settings.TaskbarExtraInfo = TaskbarExtraInfo.Percent;
+                    break;
+                case 2:
+                    AppSettings.Settings.TaskbarExtraInfo = TaskbarExtraInfo.CurrentSpeed;
+                    break;
+                case 3:
+                    AppSettings.Settings.TaskbarExtraInfo = TaskbarExtraInfo.RemainingTime;
+                    break;
+                case 4:
+                    AppSettings.Settings.TaskbarExtraInfo = TaskbarExtraInfo.ActiveDevice;
+                    break;
+                case 5:
+                    AppSettings.Settings.TaskbarExtraInfo = TaskbarExtraInfo.ImageFileName;
+                    break;
+            }
+
+            AppSettings.SaveSettings();
+            Close();
+        }
+
+        private void Window_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.Key == Key.Escape)
+            {
+                Close();
+            }
+        }
+
+        private void defaultFolderDialogButton_Click(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Forms.DialogResult result = System.Windows.Forms.DialogResult.None;
+            var dlg = new System.Windows.Forms.FolderBrowserDialog();
+            dlg.SelectedPath = string.IsNullOrEmpty(defaultFolder_userSpecifyValue.Text) ? AppSettings.Settings.DefaultFolderPath : defaultFolder_userSpecifyValue.Text;
+
+            try
+            {
+                result = dlg.ShowDialog();
+            }
+            catch
+            {
+                dlg.SelectedPath = "";
+                result = dlg.ShowDialog();
+            }
+            if (result == System.Windows.Forms.DialogResult.OK)
+            {
+                defaultFolder_userSpecifyValue.Text = dlg.SelectedPath;
+            }
         }
     }
 }
