fbc27624bbb09bf8f0ba71b07295920d0e1983ce
Filip Sikora <filipxsikora@gmail.com>
2016-05-21 17:20:04 +0200

    Extensions class added

---
+++ dotNetDiskImager/Models/Extensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNetDiskImager.Models
+{
+    public static class Extensions
+    {
+        public static void Fill(this byte[] arr, byte value)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = value;
+            }
+        }
+    }
+}
