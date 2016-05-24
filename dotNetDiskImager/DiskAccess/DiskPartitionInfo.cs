1debcd90236034d297702e65cbcd1e603b456027
Filip Sikora <filipxsikora@gmail.com>
2016-05-24 22:14:40 +0200

    Added Disk Partition Info class

---
+++ dotNetDiskImager/DiskAccess/DiskPartitionInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNetDiskImager.DiskAccess
+{
+    public enum PartitionType { RAW, MBR, GPT }
+
+    public class DiskPartitionInfo
+    {
+        public PartitionType PartitionType { get; set; }
+        public List<ulong> PartitionSizes { get; set; } = new List<ulong>();
+        public ulong UnallocatedSize { get; set; }
+        public ulong DiskTotalSize { get; set; }
+    }
+}
