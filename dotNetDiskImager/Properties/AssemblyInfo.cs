79681d16a22cf058b354bc3692ed9d15e3a6a2f4
Filip Sikora <filipxsikora@gmail.com>
2018-11-02 19:55:38 +0100

    Bump AppVersion to 0.8.958.32

--- dotNetDiskImager/Properties/AssemblyInfo.cs
+++ dotNetDiskImager/Properties/AssemblyInfo.cs
@@ -51,7 +51,7 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.7.347.27")]
-[assembly: AssemblyFileVersion("0.7.347.27")]
+[assembly: AssemblyVersion("0.8.958.32")]
+[assembly: AssemblyFileVersion("0.8.958.32")]
 [assembly: NeutralResourcesLanguage("en")]
 
